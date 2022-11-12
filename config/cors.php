c5032bda317c372e2cc81c5c4ebdc80bd500bf5d
kkznch <kkznch710md@gmail.com>
2022-11-12 18:26:58 +0900

    Lighthouseをインストールした

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*',
+        'graphql',
+        'sanctum/csrf-cookie',
+    ],
 
     'allowed_methods' => ['*'],
 
